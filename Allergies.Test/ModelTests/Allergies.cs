218170ca3b1b7af02cfd50a61ec91ffdad97aad4
Christine Frank and joel stockamp <joel@>
2019-10-01 08:23:09 -0700

    create a basic file structure

---
+++ Allergies.Test/ModelTests/Allergies.cs
@@ -0,0 +1,23 @@
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using ToDoList.Models;
+using System;
+
+namespace Allergies.Tests
+{
+    [TestClass]
+    public class ItemTest
+    {
+
+        [TestMethod]
+        public void MethodName_WhatMethodDoes_ExpectedOutput()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
---
+++ Allergies/Models/Allergies.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using System;
+namespace Allergies.Models
+{
+    public class ClassName
+    {
+        
+    }
+}
